542e35cb0db0fdb7812b92db642e8b5dcf5eb80b
David Wright <3045209@ads.qub.ac.uk>
2024-07-29 15:47:21 +0100

    Amended criteria for 4T-BIG and fibrosis cohorts. Notes released.

--- code/4T-BIG-cohort-construction.R
+++ code/4T-BIG-cohort-construction.R
@@ -1,4 +1,4 @@
-# Cohort construction for 4T-BIG project 
+# Cohort construction for 4T-BIG and fibrosis projects
 
 library(tidyverse)
 library(lubridate)
@@ -8,6 +8,26 @@ source(rprojroot::find_rstudio_root_file("code/BIRAX-assemble-cohort.R"))
 
 # Prepare dataset
 
+
+# Details of all the examinations that were extracted
+# Note that there were multiple ExamIDs and SeriesIDs (scans within exams) on some dates
+oct_details <- fread(file = file.path(file_path, "OCT_ImageVariables.txt"))
+oct_details[, c("StartCoordX", "StartCoordY") := lapply(.SD, as.numeric), .SDcols = c("StartCoordX", "StartCoordY")]
+setkey(oct_details, FilePath)
+
+# List of all dates on which an OCT volume scan was taken 
+# When there are multiple exams on the same day, select the last (matches criteria used for raw NOA data)
+# Assumes there was a problem with the earlier scan
+oct_volumes <- unique(oct_details[ModalityType == "Volume" & ModalityProcedure == "IR_OCT" & ImageType == "OCT",], by = c("PatientID", "EyeCode", "ExamDate"), fromLast = TRUE)
+
+# List of all dates where an OCT multicolour photograph was taken
+oct_multicol <- unique(oct_details[ModalityType == "Single" & ModalityProcedure == "MC" & ImageType == "ANGIO",], by = c("PatientID", "EyeCode", "ExamDate"), fromLast = TRUE)
+
+# List of all dates where a fluorsecein angiongram (OCT) was taken
+oct_fa <- unique(oct_details[(ModalityProcedure == "FA_OCT" & ModalityType == "Volume"),], by = c("PatientID", "EyeCode", "ExamDate"), fromLast = TRUE)
+# (ModalityProcedure == "FA" & ModalityType == "SteroPair")
+
+  
 # For each patient calculate the time since the first injection in either eye (first eye index_date)
 index_dates_4t <- as.data.table(injection_summary_eye)[, .(index_date = min(index_date)), by = PatientID]
 
@@ -25,14 +45,8 @@ visits_4t[, visit_sequence := visit_number - visit_number[index_visit], by = Pat
 # Calculate number of months since the index visit
 visits_4t[, months_since_index := interval(index_date, EncounterDate)/dmonths()]
 
-
-visits_4t[PatientID == "266211D0-2020-3632-F2B5-EEF012D66060"]
-
-# Exclude patients with neither eye diagnosed with AMD at any point
-# Flag eyes with AMD diagnosis at any point
-# left_join(AMD_patients %>% 
-#             distinct(PatientID) %>% 
-#             mutate(exclude_no_AMD = FALSE), by = "PatientID")
+# Note that some visits did not have any imaging
+encounters_raw[PatientID == "FC9B5F4A-EE14-2E67-8E4E-EB5C162C6D71" & EncounterDate >= "2017-07-22"][order(EncounterDate)]
 
 
 # Put all measurements taken in each visit together
@@ -73,20 +87,22 @@ visits_4t %>%
   ,by = c("PatientID", "EncounterDate")) %>% 
   
   left_join(
-    # Indicate if fluorescein angiogram was taken
+    # Indicate if fluorescein angiogram was taken (either OCT or 2D)
     dcast(oct_fa[, .(PatientID, EyeCode = paste0("fa_", EyeCode), EncounterDate = ExamDate, fa = 1)],
           PatientID + EncounterDate ~ EyeCode, fill = 0, value.var = "fa") 
   ,by = c("PatientID", "EncounterDate")) %>% 
   
-  mutate(across(matches("_L$|_R$"), ~if_else(is.na(.), 0, .)))
+  mutate(across(matches("_L$|_R$"), ~if_else(is.na(.), 0, .))) %>% 
 
+  # Drop visits where not VA, injection or imaging took place (may have been booking visits)
+  # This will break the visit sequences for each patient
+  filter(!(inj_L == 0 & inj_R == 0 & 
+             va_L == 0 & va_R == 0 & 
+             oct_L == 0 & oct_R == 0 & 
+             multicol_L == 0 & multicol_R == 0 & 
+             fa_L == 0 & fa_R == 0))
+ 
 
-
-# visits_4t_summary[index_visit == TRUE & inj_L == 1] %>% 
-#   count(amd_L)
-#   count(va_L, oct_L, multicol_L, fa_L)
-#   # count(inj_L, inj_R)
-#   count(inj_L, inj_R, fa_L, fa_R)
 
 # Active nAMD at baseline
 # This will always contain the first eye or both if a bilateral case
@@ -185,8 +201,160 @@ patients_7yr <- distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & o
 fwrite(patients_7yr, find_rstudio_root_file("data", "output", "7yr_followup.csv"))
 
 
+### Cohorts
+
+## 4T-BIG study
+
+# Only include patients with a visit at exactly 3 years
+
+patients_3yr_exact <- distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+  visits_4t_summary, on = "PatientID", nomatch =0][
+    months_since_index >=36 & months_since_index < 37 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+      visits_4t_summary, on = "PatientID", nomatch = 0
+    ][, months_since_index := floor(months_since_index)][,
+                                                         .(visits = .N,
+                                                           injections = sum(inj_L+inj_R),
+                                                           va_measurements = sum(va_L + va_R),
+                                                           oct = sum(oct_L + oct_R),
+                                                           multicolour = sum(multicol_L + multicol_R),
+                                                           fa = sum(fa_L + fa_R)),
+                                                         by = .(months_since_index, index_visit)][
+                                                           order(months_since_index)
+                                                         ][
+                                                           months_since_index <=36 ] 
+
+# fwrite(patients_3yr_exact, find_rstudio_root_file("data", "output", "3yr-followup-exact.csv"))
 
 
+## Fibrosis project
+# Only include patients with a visit at exactly 7 years
+# Patietns with a valid baseline (injection and OCT in either eye)
+patients_7yr_exact <- distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+  # Joined to visit summary
+  visits_4t_summary, on = "PatientID", nomatch =0][
+    # Just patients with a visit in the 84th month (double)
+    months_since_index >=84 & months_since_index < 85 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+      visits_4t_summary, on = "PatientID", nomatch = 0
+    ][
+      # Convert time since index to integer
+      , months_since_index := floor(months_since_index)][
+        # Calculate number of visits/images by month
+        ,
+                                                         .(visits = .N,
+                                                           injections = sum(inj_L+inj_R),
+                                                           va_measurements = sum(va_L + va_R),
+                                                           oct = sum(oct_L + oct_R),
+                                                           multicolour = sum(multicol_L + multicol_R),
+                                                           fa = sum(fa_L + fa_R)),
+                                                         by = .(months_since_index, index_visit)][
+                                                           order(months_since_index)
+                                                         ][
+                                                           # Retain only first 7 years of followup
+      months_since_index <=84 ] 
+# fwrite(patients_7yr_exact, find_rstudio_root_file("data", "output", "7yr-followup-exact.csv"))
 
 
+## Other potential cohorts
+
+# Only include patients with a visit at exactly 4 years
+# Patietns with a valid baseline (injection and OCT in either eye)
+patients_4yr_exact <- distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+  # Joined to visit summary
+  visits_4t_summary, on = "PatientID", nomatch =0][
+    # Just patients with a visit in the 48th month (double)
+    months_since_index >=48 & months_since_index < 49 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+      visits_4t_summary, on = "PatientID", nomatch = 0
+    ][
+      # Convert time since index to integer
+      , months_since_index := floor(months_since_index)][
+        # Calculate number of visits/images by month
+        ,
+        .(visits = .N,
+          injections = sum(inj_L+inj_R),
+          va_measurements = sum(va_L + va_R),
+          oct = sum(oct_L + oct_R),
+          multicolour = sum(multicol_L + multicol_R),
+          fa = sum(fa_L + fa_R)),
+        by = .(months_since_index, index_visit)][
+          order(months_since_index)
+        ][
+          # Retain only first 7 years of followup
+          months_since_index <=48 ] 
+# fwrite(patients_4yr_exact, find_rstudio_root_file("data", "output", "4yr-followup-exact.csv"))
+
+
+# Only include patients with a visit at exactly 5 years
+# Patietns with a valid baseline (injection and OCT in either eye)
+patients_5yr_exact <- distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+  # Joined to visit summary
+  visits_4t_summary, on = "PatientID", nomatch =0][
+    # Just patients with a visit in the 60th month (double)
+    months_since_index >=60 & months_since_index < 61 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+      visits_4t_summary, on = "PatientID", nomatch = 0
+    ][
+      # Convert time since index to integer
+      , months_since_index := floor(months_since_index)][
+        # Calculate number of visits/images by month
+        ,
+        .(visits = .N,
+          injections = sum(inj_L+inj_R),
+          va_measurements = sum(va_L + va_R),
+          oct = sum(oct_L + oct_R),
+          multicolour = sum(multicol_L + multicol_R),
+          fa = sum(fa_L + fa_R)),
+        by = .(months_since_index, index_visit)][
+          order(months_since_index)
+        ][
+          # Retain only first 7 years of followup
+          months_since_index <=60 ] 
+# fwrite(patients_5yr_exact, find_rstudio_root_file("data", "output", "5yr-followup-exact.csv"))
+
+# Only include patients with a visit at exactly 6 years
+# Patietns with a valid baseline (injection and OCT in either eye)
+patients_6yr_exact <- distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+  # Joined to visit summary
+  visits_4t_summary, on = "PatientID", nomatch =0][
+    # Just patients with a visit in the 72nd month (double)
+    months_since_index >=72 & months_since_index < 73 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+      visits_4t_summary, on = "PatientID", nomatch = 0
+    ][
+      # Convert time since index to integer
+      , months_since_index := floor(months_since_index)][
+        # Calculate number of visits/images by month
+        ,
+        .(visits = .N,
+          injections = sum(inj_L+inj_R),
+          va_measurements = sum(va_L + va_R),
+          oct = sum(oct_L + oct_R),
+          multicolour = sum(multicol_L + multicol_R),
+          fa = sum(fa_L + fa_R)),
+        by = .(months_since_index, index_visit)][
+          order(months_since_index)
+        ][
+          # Retain only first 7 years of followup
+          months_since_index <=72 ] 
+# fwrite(patients_6yr_exact, find_rstudio_root_file("data", "output", "6yr-followup-exact.csv"))
+
+
+# 
+# distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+#        visits_4t_summary, on = "PatientID", nomatch =0][
+#              months_since_index >=84 & months_since_index < 85 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+#                    visits_4t_summary, on = "PatientID", nomatch = 0
+#                ][PatientID == "FC9B5F4A-EE14-2E67-8E4E-EB5C162C6D71"]
+# 
+# encounters_raw[PatientID == "FC9B5F4A-EE14-2E67-8E4E-EB5C162C6D71" & EncounterDate >= "2017-07-22"][order(EncounterDate)]
+
+
+# 
+# 
+# distinct(namd_baseline[(inj_R == T & oct_R == 1)|(inj_L == T & oct_L == 1),.(PatientID)][
+#   visits_4t_summary, on = "PatientID", nomatch =0][
+#     months_since_index >=84 & months_since_index < 85 & (oct_L == 1 | oct_R == 1), .(PatientID)])[
+#       visits_4t_summary, on = "PatientID", nomatch = 0
+#     ][
+#       months_since_index >=84 & months_since_index <85 ] 
+# 
+# encounters_raw[PatientID == "0AE95C3D-873D-B865-346F-2502DFCFD372" & EncounterDate >= "2018-06-02" & EncounterDate <= "2018-06-16",]
+# 
 
